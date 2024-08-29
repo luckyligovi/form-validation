99511ad2d00b2c2b464c567ccbc08ea99a283581
luckyligovi <lucky.luvihi@student.moringaschool.com>
2024-08-29 17:39:19 +0300

    form styled

---
+++ form.js
@@ -0,0 +1,4 @@
+let nameError = document.getElementById("name-error")
+let emailError = document.getElementById("email-error")
+let phoneError = document.getElementById("phone-error")
+let submitError = document.getElementById("submit-error")
